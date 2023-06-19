7e5cff40a80d4bf0ff636898555579b3850db8e5
rhenium <rhenium750@gmail.com>
2023-06-19 22:36:27 +0900

    fix

--- src/core/main/samples/helloWorld/index.tsx
+++ src/core/main/samples/helloWorld/index.tsx
@@ -3,7 +3,7 @@ import { Canvas, Image, Text } from "../../../unit/package/standardUix/main";
 export const App = () => {
   return (
     <Canvas size={[1000, 1000]}>
-      <Image color={[0.5, 0.5, 0.5, 1]} />
+      <Image tint={[0.5, 0.5, 0.5, 1]} />
       <Text content="HelloWorld!" />
     </Canvas>
   );
